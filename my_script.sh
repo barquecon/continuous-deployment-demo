22e700638c7a49f14b0ff2a51709eaec21d82731
Anand Bholane <anand.bholane@barquecontech.com>
2021-10-21 19:45:39 +0530

    Travis file updated

--- my_script.sh
+++ my_script.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-sshpass -p "Barquecon@123" ssh barquecontechnologiespvtltd@35.225.60.29
-cd /var/www/html
-git pull origin master
+sshpass -p "Barquecon@123" ssh barquecontechnologiespvtltd@35.225.60.29 'cd /var/www/html; sudo git pull origin master'

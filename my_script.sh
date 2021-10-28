038b2bfb6f4013059bfaf8124a4eb4b5e51963c6
Anand Bholane <anand.bholane@barquecontech.com>
2021-10-28 11:57:24 +0530

    Travis and Script file changes

--- my_script.sh
+++ my_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sshpass -p "Barquecon@123" ssh barquecontechnologiespvtltd@35.225.60.29 'cd /var/www/html; sudo git pull origin master'
+sshpass -p "Barquecon@123" ssh barquecontechnologiespvtltd@35.222.190.100 'cd /var/www/html; sudo git pull origin master'
